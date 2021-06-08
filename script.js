9cb0fde6674c558153b9193870c6e991e80bcbce
Jucian Decezare <63060456+JucianKaue@users.noreply.github.com>
2021-06-08 13:20:59 -0300

    V.08 - Mouse click working

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 window.document.addEventListener('DOMContentLoaded', setup)
 
+// DEFINE AS CONFIGURAÇÕES DA TELA
+let screen = [1200, 500]
 
+// DEFINE AS CONFIGURAÇÕES DAS CASAS
+let cell_size = 20
+let cell_border_size = 0.5
 
+// Define a quantidade de linha e coluna -> tamanho do tabuleiro/tamanho da casa - soma de todas as bordas arredodada para cima.
+let columns = screen[0]/cell_size - (Math.round((screen[0]/cell_size*cell_border_size)/cell_size))
+let rows = screen[1]/cell_size - (Math.round((screen[1]/cell_size*cell_border_size)/cell_size))
 
-/*
-&#9633 -> quadrado branco
-&#9632 -> quadrado preto
-*/
+function setup() {
+    // Verificar se o HTMl canvas está funcionando corretamente e inicializa-lo
+    let canvas = window.document.querySelector("canvas")
+    if (! canvas.getContext) {
+        window.alert("CANVAS NÃO INICIADO CORRETAMENTR")
+    } else {
+        ctx = canvas.getContext('2d')
+    }
+    
+    let grid = Create_2D_grid(columns, rows)    // Cria o grid
+    draw(grid) // Desenha o tabuleiro
+    
+    canvas.addEventListener('click', (event) => {
+        let x = event.offsetX
+        let y = event.offsetY
 
+        let cell = convert_PosToCell(x, y)
+
+        add_cell(cell, grid)
+    })
+
+    // LOOP DO JOGO
+    let frame = 200
+    let game_run
+    let game = function() {
+        grid = actualize_grid(grid)
+        draw(grid)
+        if (game_run) {
+           setTimeout(game, frame) 
+        }
+        
+    }
+
+    // BOTÕES
+    // Botão "start" para iniciar o jogo
+    window.document.querySelector('button#start').addEventListener('click', () => {
+        game_run = true
+        setTimeout(game, frame)
+    })
+    // Botão "pause" para pausar o jogo
+    window.document.querySelector('button#pause').addEventListener('click', () => {
+        game_run = false
+    })
+
+}
 
 // Creates an array of arrays to represents a 2D grid
 function Create_2D_grid(x, y) {
@@ -161,110 +209,25 @@ function actualize_grid(grid) {
     return new_grid
 }
 
-// ERRO
-function click(pos, grid) {
+// Convert the mouse coordinates to the grid coordinates
+function convert_PosToCell(x, y) {
+    let border_sizeX = (screen[0] - ((columns*cell_size)+((columns-1)*cell_border_size))) / 2
+    let border_sizeY= (screen[1] - ((rows*cell_size)+((rows-1)*cell_border_size))) / 2
+
+    let cellX = Math.trunc((x - border_sizeX) / (cell_size+cell_border_size))
+    let cellY = Math.trunc((y - border_sizeY) / (cell_size+cell_border_size))
+
+    return [cellX, cellY]
+}
+
+// Chance the life status of the cell
+function add_cell(pos, grid) {
     console.log(pos)
-    let cell = convert_PosToCell(pos)
-    grid[cell[1]][cell[0]] = 1
-    draw(grid)
-}
-// ERRO
-function convert_PosToCell(mouse_position) {
-    let x_space = (screen[0] - ((cell_size+cell_border_size)*columns)) / 2
-    let y_space = (screen[1] - ((cell_size+cell_border_size)*rows)) / 2
-
-    let CellBorderSpace_X = ((mouse_position[0] - x_space)/(20.5))
-    let CellBorderSpace_Y
-    //let CellBorderSpace_X = (((mouse_position[1]-x_space)/(cell_size))*cell_border_size)
-    //let CellBorderSpace_Y = (((mouse_position[0]-y_space)/(cell_size))*cell_border_size)
-
-    let cell_X = Math.trunc((mouse_position[0] - (x_space - CellBorderSpace_X)) / cell_size)
-    let cell_Y = Math.trunc((mouse_position[1] - (y_space - CellBorderSpace_Y)) / cell_size)
-
-        
-    console.log(CellBorderSpace_X, CellBorderSpace_Y)
-    return [cell_X, cell_Y]
-
-    /*
-    let x_space = (screen[0] - ((cell_size+cell_border_size)*columns)) / 2
-    let y_space = (screen[1] - ((cell_size+cell_border_size)*rows)) / 2
-
-    let newMousePosX =  mouse_position[0] + (cell_border_size*columns) - x_space
-
-    let cel_x = parseInt((newMousePosX) / (cell_size))
-    let cel_y = parseInt((mouse_position[1] - y_space) / (cell_size))
-
-    let cell_x = cel_x 
-    let cell_y = cel_y
-
-
-    let y = screen[0]/cell_size - (Math.round((screen[0]/cell_size*cell_border_size)/cell_size))
-    let x = screen[1]/cell_size - (Math.round((screen[1]/cell_size*cell_border_size)/cell_size))
-
-    return [cell_x, cell_y]
-    */
-}
-// FUNÇÃO TESTE, EXCLUIR DPS
-function change_grid(grid) {
-    grid[6][4] = 1
-    grid[6][5] = 1
-    grid[7][6] = 1
-    grid[8][5] = 1
-    grid[8][4] = 1
-    grid[7][3] = 1
-    grid[7][2] = 1
-    return grid
-}
-
-
-// DEFINE AS CONFIGURAÇÕES DA TELA
-let screen = [1200, 500]
-
-// DEFINE AS CONFIGURAÇÕES DAS CASAS
-let cell_size = 20
-let cell_border_size = 0.5
-
-// Define a quantidade de linha e coluna -> tamanho do tabuleiro/tamanho da casa - soma de todas as bordas arredodada para cima.
-let columns = screen[0]/cell_size - (Math.round((screen[0]/cell_size*cell_border_size)/cell_size))
-let rows = screen[1]/cell_size - (Math.round((screen[1]/cell_size*cell_border_size)/cell_size))
-
-canvas.addEventListener('click', click([canvas.offsetWidth, canvas.offsetHeight]))
-
-function setup() {
-    // Verificar se o HTMl canvas está funcionando corretamente e inicializa-lo
-    let canvas = window.document.querySelector("canvas")
-    if (! canvas.getContext) {
-        window.alert("CANVAS NÃO INICIADO CORRETAMENTR")
+    if (grid[pos[1]][pos[0]] == 1) {
+        grid[pos[1]][pos[0]] = 0
     } else {
-        ctx = canvas.getContext('2d')
+        grid[pos[1]][pos[0]] = 1
     }
     
-    let grid = Create_2D_grid(columns, rows)    // Cria o grid
-    draw(grid) // Desenha o tabuleiro
-    
-    grid = change_grid(grid)
     draw(grid)
-    
-    // loop
-    let frame = 200
-    let game_run
-    let game = function() {
-        grid = actualize_grid(grid)
-        draw(grid)
-        if (game_run) {
-           setTimeout(game, frame) 
-        }
-        
-    }
-    // BOTÕES
-    // Botão "start" para iniciar o jogo
-    window.document.querySelector('button#start').addEventListener('click', () => {
-        game_run = true
-        setTimeout(game, frame)
-    })
-    // Botão "pause" para pausar o jogo
-    window.document.querySelector('button#pause').addEventListener('click', () => {
-        game_run = false
-    })
-
 }
